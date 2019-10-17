fc8a049bd9c9814ddedafbd98649fb9a79d4f990
Twiddly <pew@pewpew.moe>
2019-10-17 17:15:42 +0200

    add dist folder;

---
+++ dist/precache-manifest.25e4a0c64faa3e071185e6a23ecb9c31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6307a1aa546fa068042fa280ac577f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0519713110d531dae1c9",
+    "url": "/static/css/main.7aa68b70.chunk.css"
+  },
+  {
+    "revision": "3b74a132826938c57533",
+    "url": "/static/js/2.bfe8799d.chunk.js"
+  },
+  {
+    "revision": "0519713110d531dae1c9",
+    "url": "/static/js/main.685428c9.chunk.js"
+  },
+  {
+    "revision": "5bd62437fed5268ab556",
+    "url": "/static/js/runtime-main.fa63738d.js"
+  },
+  {
+    "revision": "2f9b3d92b5743204916f6409f8ecbce7",
+    "url": "/static/media/happypandax.2f9b3d92.svg"
+  }
+]);
